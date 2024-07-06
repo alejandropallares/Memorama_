b335fa6ccbd7f93b85b16ccb6b11e406520d4db8
alejandropallares <alejandropallares10gmail.com>
2024-07-05 21:36:34 -0600

    Initial Commit

---
+++ Memorama_/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorama_
+//
+//  Created by Alejandro Pallares on 05/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorama_/Memorama_App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorama_App.swift
+//  Memorama_
+//
+//  Created by Alejandro Pallares on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorama_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
